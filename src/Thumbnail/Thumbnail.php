25e194dff74316c4e9af7687f58eac74dbc0946a
Hylin Yin <hylin@iphp8.com>
2016-07-19 11:55:42 +0800

    Project initialization.

---
+++ src/Thumbnail/Thumbnail.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * This file is part of the Hymate package.
+ *
+ * (c) Hylin Yin <hylin@iphp8.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Hymate\Thumbnail;
+
+/**
+ * Provides basic utility to generate the thumbnail.
+ *
+ * @package Hymate\Thumbnail
+ * @author Hylin Yin <hylin@iphp8.com>
+ */
+class Thumbnail
+{
+
+}
